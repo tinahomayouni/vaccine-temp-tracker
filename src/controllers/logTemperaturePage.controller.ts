4271d9a82c45baa9392a018c7b561fb8e3ad0f7d
Tina Homayouni <t.homayouni@vardastapp.com>
2023-09-16 02:44:35 +0330

    add error handling by send data paramet to ejs / add data in database / change date value to string as entity expect /change path for sqlite.db

--- src/controllers/logTemperaturePage.controller.ts
+++ src/controllers/logTemperaturePage.controller.ts
@@ -1,6 +1,7 @@
-// src/controllers/logTempreturePage.controller.ts
-import { Body, Controller, Get, Post, Render } from '@nestjs/common';
+// src/controllers/logTemperaturePage.controller.ts
+import { Body, Controller, Get, Post, Render, Req, Res } from '@nestjs/common';
 import { LogTempreturePageService } from '../services/logTemperaturePage.service';
+import { Response, Request } from 'express';
 
 @Controller('logTempreturePage')
 export class LogTempreturePageController {
@@ -10,12 +11,25 @@ export class LogTempreturePageController {
 
   @Get()
   @Render('logTempreturePage') // Specify the name of your EJS template (e.g., logTempreturePage.ejs)
-  async getLogTemperaturePage() {
-    return {};
+  async getLogTemperaturePage(@Req() req: Request) {
+    const success = req.query.success === 'true';
+    return { success };
   }
+
   @Post('/submit')
-  async saveTemperature(@Body('vaccineTemperature') value: number) {
-    await this.logTempreturePageService.saveTemperature(value);
-    return { message: 'Temperature data saved successfully' };
+  async saveTemperature(
+    @Body('vaccineTemperature') value: number,
+    @Res() res: Response,
+  ) {
+    try {
+      // Save the temperature data using your service
+      await this.logTempreturePageService.saveTemperature(value);
+
+      // Redirect to the success page with a query parameter
+      res.redirect('/logTempreturePage?success=true');
+    } catch (error) {
+      // Redirect to the error page with a query parameter
+      res.redirect('/logTempreturePage?error=true');
+    }
   }
 }
--- src/services/logTemperaturePage.service.ts
+++ src/services/logTemperaturePage.service.ts
@@ -3,15 +3,22 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Temperature } from '../entities/tempreture.entity';
+
 @Injectable()
 export class LogTempreturePageService {
   constructor(
     @InjectRepository(Temperature)
     private readonly temperatureRepository: Repository<Temperature>,
   ) {}
+
   async saveTemperature(value: number): Promise<Temperature> {
+    console.log('Service: Saving temperature data');
+
     const temperature = new Temperature();
     temperature.value = value;
+
+    // Convert the Date object to a formatted string (e.g., ISO string)
+    temperature.timestamp = new Date().toISOString();
 
     return this.temperatureRepository.save(temperature);
   }
