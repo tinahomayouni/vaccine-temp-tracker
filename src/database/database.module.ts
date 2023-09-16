4a463161555cf1dec36f2ed762a5e9a531929489
Tina Homayouni <t.homayouni@vardastapp.com>
2023-09-16 18:32:03 +0330

    check database tables and values (work correctly azizam) , change foldering to standard structure

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -7,7 +7,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite', // Set the database type to SQLite
-      database: 'src/database/sqlite.db', // Provide the path to your SQLite database file
+      database: 'database/sqlite.db', // Provide the path to your SQLite database file
       synchronize: true, // Auto-create database schema (for development only)
       entities: [__dirname + '/../**/*.entity{.ts,.js}'], // Entity files location
     }),
