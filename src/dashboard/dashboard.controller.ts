9bc60e753ebc6c6d12e2ff48170ffe511f96cd8b
Tina Homayouni <t.homayouni@vardastapp.com>
2023-09-20 17:01:51 +0330

    add OPENAPI-swagger

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,17 +1,17 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
-
+import { ApiTags } from '@nestjs/swagger';
+@ApiTags('dashboard1') // Specify the tag for this controller
 @Controller('dashboard')
 export class DashboardController {
   constructor(private readonly dashboardService: DashboardService) {}
 
-  @Get()
+  @Get() //http://localhost:3000/api
   @Render('dashboard')
   async getDashboardView() {
     const temperatures = await this.dashboardService.getRecentReadings();
     return { temperatures };
   }
-
   @Get('alarms')
   async getAlarms() {
     const temperatures = await this.dashboardService.getAlarms();
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,20 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { join } from 'path';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Vaccine Temperature Tracker')
+    .setDescription('App for Track Vaccine Temperature')
+    .setVersion('1.0')
+    .addTag('trackerAPI') // Add tags for your controllers
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
